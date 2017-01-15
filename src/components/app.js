aaa49549cd09e8b0a02f3d8c2e169c95c67c2d99
lgants <lgants@gmail.com>
2017-01-15 17:00:47 -0600

    authentication reducer

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
+import Header from './header';
 
+// { this.props.children } is from react-router; essentially if app component has any children and a user visits a route that matches up to those children, app will show those children
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        { this.props.children }
+      </div>
     );
   }
 }
